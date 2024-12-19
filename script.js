32c3214489f39ba2fc0adebade0fcf48fe43da27
rehano1 <rehan.ashraf179@gmail.com>
2024-12-19 09:50:19 +0500

    complete logic

--- script.js
+++ script.js
@@ -3,27 +3,18 @@ const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('.btn');
 
 
-console.log('yesy')
-buttons.addEventListener('click', function(event) {
-    debugger
-});
+
 
 
 
 // variables for the input
-let firstNumber = '';
-let secondNumber = '';
+let firstNum = '';
+let secondNum = '';
 let operator = '';
+// flag to check if operator has been clicked
+let isOperatorClicked = false;  
 
-
-function operate ()  {
-    if (operator === '+'){
-        display
-    }
-}
-
-
-// basic functions 
+// basic arithemaic functions 
 function add (a, b)  {
     return a + b;
 }
@@ -40,3 +31,74 @@ function divide (a, b) {
     }
     return a / b;   
 }
+
+// to call basic function
+
+function operate (operator,num1, num2) {
+    switch (operator) {
+        case "+" :
+            return add(num1, num2);
+        case "-" :
+            return subtract(num1, num2);
+        case "*" : 
+            return multiply(num1, num2);
+        case "/" : 
+            return divide(num1, num2);
+        default: 
+            return "Error: invalid operator";
+        
+    }
+}
+
+
+
+// Store and display the value
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const value = button.value;
+        
+        // Handle Clear
+        if (value == 'C') {
+            firstNum = '';
+            secondNum = '';
+            operator = '';
+            display.value = "0"
+            isOperatorClicked = false;
+            return;
+        }
+        // handle equals
+        if (value == '=') {
+            if(firstNum && operator && secondNum) {
+                const result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
+                display.value = result;
+                // Reset after calculation
+                firstNum = result.toString();
+                secondNum = '';
+                operator = '';
+                isOperatorClicked = false;
+            }
+            return;
+        }
+
+    // handle operators
+    if (["+", "-", "*", "/"].includes(value)) {
+        if (firstNum && !isOperatorClicked) {
+            operator = value;
+            isOperatorClicked = true;
+            display.value = value;
+        }
+        return;
+    }
+    // handle operator inputs 
+    if (!isOperatorClicked) {
+        firstNum += value;
+        display.value = firstNum;
+    } else {
+        secondNum += value;
+        display.value = secondNum;
+    }   
+    });
+});
+
+
